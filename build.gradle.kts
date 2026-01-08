90fc8bf4702eb137d2454506d1b042e60b3f62e5
Jamy Bailly <jamy.bailly@infomaniak.com>
2026-01-08 08:05:02 +0100

    feat: Add target / compile / java version and variant

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,14 @@
-import org.gradle.kotlin.dsl.kotlin
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    extra.apply {
+        set("appCompileSdk", 36)
+        set("appTargetSdk", 36)
+        set("appMinSdk", 27)
+        set("legacyMinSdk", 27)
+        set("javaVersion", JavaVersion.VERSION_17)
+    }
+}
+
 plugins {
     alias(core.plugins.android.application) apply false
     alias(core.plugins.kotlin.android) apply false
