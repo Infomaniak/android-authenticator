5e6bc78d92f46a385011c5cb14ea2c6cace5a1cd
Jamy Bailly <jamy.bailly@infomaniak.com>
2026-01-15 13:40:47 +0100

    feat: Add crossapplogin + hilt + minimum Auth and UserDatabase

--- app/src/main/kotlin/com/infomaniak/auth/ui/screen/onboarding/OnboardingScreen.kt
+++ app/src/main/kotlin/com/infomaniak/auth/ui/screen/onboarding/OnboardingScreen.kt
@@ -17,22 +17,65 @@
  */
 package com.infomaniak.auth.ui.screen.onboarding
 
-import androidx.compose.foundation.layout.Box
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.infomaniak.auth.ui.theme.AuthenticatorTheme
+import com.infomaniak.core.crossapplogin.back.BaseCrossAppLoginViewModel.AccountsCheckingState
+import com.infomaniak.core.crossapplogin.back.BaseCrossAppLoginViewModel.AccountsCheckingStatus
+import com.infomaniak.core.crossapplogin.back.ExternalAccount
+import com.infomaniak.core.crossapplogin.front.components.CrossLoginBottomContent
+import com.infomaniak.core.crossapplogin.front.components.NoCrossAppLoginAccountsContent
+import com.infomaniak.core.crossapplogin.front.data.CrossLoginDefaults
+import com.infomaniak.core.crossapplogin.front.previews.AccountsPreviewParameter
 import com.infomaniak.core.onboarding.OnboardingScaffold
+import com.infomaniak.core.onboarding.components.OnboardingComponents
+import com.infomaniak.core.ui.compose.basics.CallableState
 import com.infomaniak.core.ui.compose.preview.PreviewSmallWindow
 
 @Composable
-fun OnboardingScreen(navigateToHome: () -> Unit) {
+fun OnboardingScreen(
+    crossAppLoginViewModel: CrossAppLoginViewModel = viewModel(),
+    loginRequest: CallableState<List<ExternalAccount>>,
+    navigateToHome: () -> Unit
+) {
+    val accountsCheckingState by crossAppLoginViewModel.accountsCheckingState.collectAsStateWithLifecycle()
+    val skippedIds by crossAppLoginViewModel.skippedAccountIds.collectAsStateWithLifecycle()
+
+    val hostActivity = LocalActivity.current as ComponentActivity
+    LaunchedEffect(crossAppLoginViewModel) {
+        crossAppLoginViewModel.activateUpdates(hostActivity)
+    }
+
+    OnboardingScreen(
+        accountsCheckingState = { accountsCheckingState },
+        skippedIds = { skippedIds },
+        isLoginButtonLoading = { loginRequest.isAwaitingCall.not() },
+        onLoginRequest = { accounts -> loginRequest(accounts) },
+        onSaveSkippedAccounts = { crossAppLoginViewModel.skippedAccountIds.value = it },
+        onStartClicked = navigateToHome,
+    )
+}
+
+@Composable
+private fun OnboardingScreen(
+    accountsCheckingState: () -> AccountsCheckingState,
+    skippedIds: () -> Set<Long>,
+    isLoginButtonLoading: () -> Boolean,
+    onLoginRequest: (accounts: List<ExternalAccount>) -> Unit,
+    onSaveSkippedAccounts: (Set<Long>) -> Unit,
+    onStartClicked: () -> Unit,
+) {
     val pagerState = rememberPagerState(pageCount = { Page.entries.size })
 
     OnboardingScaffold(
@@ -41,11 +84,24 @@ fun OnboardingScreen(navigateToHome: () -> Unit) {
             page.toOnboardingPage(pagerState, index)
         },
         bottomContent = { paddingValues ->
-            BottomContent(
+            OnboardingComponents.CrossLoginBottomContent(
                 modifier = Modifier
                     .padding(paddingValues)
                     .consumeWindowInsets(paddingValues),
-                navigateToHome = navigateToHome
+                pagerState = pagerState,
+                accountsCheckingState = accountsCheckingState,
+                skippedIds = skippedIds,
+                isLoginButtonLoading = isLoginButtonLoading,
+                customization = CrossLoginDefaults.customize(
+                    colors = CrossLoginDefaults.colors(
+                        titleColor = MaterialTheme.colorScheme.primary,
+                        descriptionColor = MaterialTheme.colorScheme.secondary
+                    ),
+                ),
+                onContinueWithSelectedAccounts = { selectedAccounts -> onLoginRequest(selectedAccounts) },
+                onUseAnotherAccountClicked = { onLoginRequest(emptyList()) },
+                onSaveSkippedAccounts = onSaveSkippedAccounts,
+                noCrossAppLoginAccountsContent = NoCrossAppLoginAccountsContent.accountOptional { onStartClicked() }
             )
         }
     )
@@ -53,24 +109,19 @@ fun OnboardingScreen(navigateToHome: () -> Unit) {
 
 @PreviewSmallWindow
 @Composable
-private fun OnboardingScreenPreview() {
-    AuthenticatorTheme {
-        OnboardingScreen(navigateToHome = {})
-    }
-}
-
-@Composable
-private fun BottomContent(
-    modifier: Modifier = Modifier,
-    navigateToHome: () -> Unit
+private fun OnboardingScreenPreview(
+    @PreviewParameter(AccountsPreviewParameter::class) accounts: List<ExternalAccount>
 ) {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = modifier
-            .fillMaxWidth()
-    ) {
-        Button(onClick = navigateToHome) {
-            Text("Go Home")
-        }
+    AuthenticatorTheme {
+        OnboardingScreen(
+            accountsCheckingState = {
+                AccountsCheckingState(AccountsCheckingStatus.Checking, checkedAccounts = accounts)
+            },
+            skippedIds = { emptySet() },
+            isLoginButtonLoading = { true },
+            onLoginRequest = {},
+            onSaveSkippedAccounts = {},
+            onStartClicked = {},
+        )
     }
 }
