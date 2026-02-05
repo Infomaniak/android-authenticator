f0de04aa439a22bae322c9a82c42a01221ce2af5
Vincent Te <vincent.te@infomaniak.com>
2026-02-05 12:55:03 +0100

    chore: Add new colors

--- app/src/main/kotlin/com/infomaniak/auth/ui/theme/CustomColor.kt
+++ app/src/main/kotlin/com/infomaniak/auth/ui/theme/CustomColor.kt
@@ -29,6 +29,17 @@ data class CustomColorScheme(
     val accountWarning: Color = Color.Unspecified,
     val accountItemBackground: Color = Color.Unspecified,
     val optionsSectionBackground: Color = Color.Unspecified,
+    val accountDisconnected: Color = Color.Unspecified,
+    val loginAgainButton: Color = Color.Unspecified,
+    val loginAgainButtonText: Color = Color.Unspecified,
+    val outline: Color = Color.Unspecified,
+    val outlineError: Color = Color.Unspecified,
+    val accountErrorLoginBackground: Color = Color.Unspecified,
+    val accountErrorLoginText: Color = Color.Unspecified,
+    val accountErrorLoginIcon: Color = Color.Unspecified,
+    val accountErrorContactSupportButton: Color = Color.Unspecified,
+    val accountErrorLoginButtonText: Color = Color.Unspecified,
+    val contactSupportButtonText: Color = Color.Unspecified,
 )
 
 private val illustrationBackgroundGradientLight = lightScheme.primary.copy(alpha = 0.28f)
@@ -45,6 +56,27 @@ private val accountItemBackgroundLight = neutral100
 private val accountItemBackgroundDark = backgroundSurface
 private val optionsSectionBackgroundLight = neutral100
 private val optionsSectionBackgroundDark = backgroundSurface
+private val accountDisconnectedLight = neutralDim4
+private val accountDisconnectedDark = neutralDim4
+private val loginAgainButtonLight = warningDim3Light
+private val loginAgainButtonDark = warningDim3Dark
+private val loginAgainButtonTextLight = warningDim6Light
+private val loginAgainButtonTextDark = Color.White
+private val outlineLight = neutral50
+private val outlineErrorLight = errorDim4Light
+private val outlineErrorDark = errorDim4Dark
+private val accountErrorLoginBackgroundLight = errorDim1Light
+private val accountErrorLoginBackgroundDark = errorDim1Dark
+private val accountErrorLoginTextLight = errorDim6Light
+private val accountErrorLoginTextDark = errorDim6Dark
+private val accountErrorLoginIconLight = errorDim5Light
+private val accountErrorLoginIconDark = errorDim5Dark
+private val accountErrorContactSupportButtonLight = errorDim3Light
+private val accountErrorContactSupportButtonDark = errorDim3Dark
+private val accountErrorLoginButtonTextLight = Color.Black
+private val accountErrorLoginButtonTextDark = Color.White
+private val contactSupportButtonTextLight = errorDim6Light
+private val contactSupportButtonTextDark = Color.White
 
 val lightCustomScheme = CustomColorScheme(
     illustrationBackgroundGradient = illustrationBackgroundGradientLight,
@@ -54,6 +86,17 @@ val lightCustomScheme = CustomColorScheme(
     accountWarning = accountWarningLight,
     accountItemBackground = accountItemBackgroundLight,
     optionsSectionBackground = optionsSectionBackgroundLight,
+    accountDisconnected = accountDisconnectedLight,
+    loginAgainButton = loginAgainButtonLight,
+    loginAgainButtonText = loginAgainButtonTextLight,
+    outline = outlineLight,
+    outlineError = outlineErrorLight,
+    accountErrorLoginBackground = accountErrorLoginBackgroundLight,
+    accountErrorLoginText = accountErrorLoginTextLight,
+    accountErrorLoginIcon = accountErrorLoginIconLight,
+    accountErrorContactSupportButton = accountErrorContactSupportButtonLight,
+    accountErrorLoginButtonText = accountErrorLoginButtonTextLight,
+    contactSupportButtonText = contactSupportButtonTextLight,
 )
 
 val darkCustomScheme = CustomColorScheme(
@@ -64,4 +107,15 @@ val darkCustomScheme = CustomColorScheme(
     accountWarning = accountWarningDark,
     accountItemBackground = accountItemBackgroundDark,
     optionsSectionBackground = optionsSectionBackgroundDark,
+    accountDisconnected = accountDisconnectedDark,
+    loginAgainButton = loginAgainButtonDark,
+    loginAgainButtonText = loginAgainButtonTextDark,
+    outline = outlineLight, // TODO No design in darkmode yet
+    outlineError = outlineErrorDark,
+    accountErrorLoginBackground = accountErrorLoginBackgroundDark,
+    accountErrorLoginText = accountErrorLoginTextDark,
+    accountErrorLoginIcon = accountErrorLoginIconDark,
+    accountErrorContactSupportButton = accountErrorContactSupportButtonDark,
+    accountErrorLoginButtonText = accountErrorLoginButtonTextDark,
+    contactSupportButtonText = contactSupportButtonTextDark,
 )
