4864362d4dac47d230f4f623be106ce6c2d314a5
Vincent Te <vincent.te@infomaniak.com>
2026-02-05 13:58:18 +0100

    feat: Add AccountDetails screen

---
+++ app/src/main/kotlin/com/infomaniak/auth/ui/screen/AccountDetails.kt
@@ -0,0 +1,330 @@
+/*
+ * Infomaniak Authenticator - Android
+ * Copyright (C) 2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.auth.ui.screen
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.infomaniak.auth.R
+import com.infomaniak.auth.ui.components.InfomaniakAuthenticatorTopAppBar
+import com.infomaniak.auth.ui.components.LargeButton
+import com.infomaniak.auth.ui.components.OptionItem
+import com.infomaniak.auth.ui.components.OptionsSection
+import com.infomaniak.auth.ui.screen.home.AccountSecurityLevel
+import com.infomaniak.auth.ui.screen.home.FakeAccount
+import com.infomaniak.auth.ui.theme.AuthenticatorTheme
+import com.infomaniak.core.ui.compose.basics.Typography
+import com.infomaniak.core.ui.compose.bottomstickybuttonscaffolds.SinglePaneScaffold
+import com.infomaniak.core.ui.compose.margin.Margin
+import com.infomaniak.core.ui.compose.preview.PreviewSmallWindow
+
+@Composable
+fun AccountDetails(account: FakeAccount, onBackPressed: () -> Unit) {
+    SinglePaneScaffold(
+        topBar = {
+            InfomaniakAuthenticatorTopAppBar(
+                withTitle = false,
+                isCentered = false,
+                onBackPressed = { onBackPressed() }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            Header(account)
+            SecurityCheck(AccountStatus.from(account.securityLevel))
+            if (account.securityLevel != AccountSecurityLevel.Secured) {
+                var hasLogin by remember { mutableStateOf(false) }
+                ActionRequired(hasLogin, logIn = {
+                    hasLogin = true
+                })
+            }
+            SettingsSections(account.securityLevel)
+        }
+    }
+}
+
+@Composable
+private fun Header(account: FakeAccount) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Avatar de l'utilisateur",
+            modifier = Modifier
+                .padding(horizontal = Margin.Medium)
+                .size(40.dp)
+                .clip(CircleShape)
+                .background(AuthenticatorTheme.materialColors.surfaceContainerHighest)
+        )
+        Column {
+            Text(text = account.name, style = Typography.h1)
+            Text(text = account.email)
+        }
+    }
+}
+
+@Composable
+private fun SecurityCheck(accountStatus: AccountStatus) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = Margin.Large)
+            .padding(horizontal = Margin.Medium)
+            .clickable(onClick = {}),
+        colors = CardDefaults.cardColors(containerColor = AuthenticatorTheme.materialColors.surfaceContainerHigh),
+        border = BorderStroke(1.dp, AuthenticatorTheme.colors.outline),
+        shape = RoundedCornerShape(16.dp),
+    ) {
+
+        Column(modifier = Modifier.padding(Margin.Small)) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    modifier = Modifier.padding(start = Margin.Small),
+                    text = stringResource(accountStatus.titleResId),
+                    style = if (accountStatus.descriptionResId != null) Typography.h2 else LocalTextStyle.current
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Icon(
+                    painter = painterResource(accountStatus.iconResId),
+                    contentDescription = null,
+                    tint = accountStatus.iconTint?.invoke() ?: AuthenticatorTheme.colors.actionRequiredBorder,
+                )
+            }
+            accountStatus.descriptionResId?.let {
+                Text(
+                    modifier = Modifier.padding(start = Margin.Small, top = Margin.Mini),
+                    text = stringResource(accountStatus.descriptionResId)
+                )
+            }
+        }
+    }
+}
+
+private data class ActionRequiredConfiguration(
+    val text: String,
+    val borderColor: Color,
+    val containerColor: Color,
+    val textColor: Color,
+    val iconColor: Color,
+    val iconRes: Int,
+)
+
+@Composable
+private fun ActionRequired(hasLogin: Boolean, logIn: () -> Unit) {
+    val configuration = if (hasLogin) {
+        ActionRequiredConfiguration(
+            text = stringResource(R.string.errorLoginFailed, 0),
+            borderColor = AuthenticatorTheme.colors.outlineError,
+            containerColor = AuthenticatorTheme.colors.accountErrorLoginBackground,
+            textColor = AuthenticatorTheme.colors.accountErrorLoginText,
+            iconColor = AuthenticatorTheme.colors.accountErrorLoginIcon,
+            iconRes = R.drawable.triangle_alert,
+        )
+    } else {
+        ActionRequiredConfiguration(
+            text = stringResource(R.string.accountNotConnectedWarningTitle),
+            borderColor = AuthenticatorTheme.colors.actionRequiredBorder,
+            containerColor = AuthenticatorTheme.colors.actionRequiredBackground,
+            textColor = Color.Unspecified,
+            iconColor = AuthenticatorTheme.colors.actionRequiredBorder,
+            iconRes = R.drawable.alert,
+        )
+    }
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = Margin.Medium)
+            .padding(top = Margin.Large),
+        colors = CardDefaults.cardColors(containerColor = configuration.containerColor),
+        border = BorderStroke(1.dp, configuration.borderColor),
+        shape = RoundedCornerShape(16.dp),
+    ) {
+        Row(modifier = Modifier.padding(Margin.Small), verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                painter = painterResource(configuration.iconRes),
+                contentDescription = null,
+                tint = configuration.iconColor,
+            )
+            Text(
+                modifier = Modifier.padding(start = Margin.Small),
+                text = configuration.text,
+                color = configuration.textColor,
+            )
+        }
+
+        if (hasLogin) ContactSupportButton()
+        LogInAgainButton(hasLogin, logIn = logIn)
+    }
+}
+
+@Composable
+private fun ContactSupportButton() {
+    LargeButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = Margin.Medium)
+            .padding(bottom = Margin.Medium),
+        title = stringResource(R.string.contactSupportTitle),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = AuthenticatorTheme.colors.accountErrorContactSupportButton,
+            contentColor = AuthenticatorTheme.colors.contactSupportButtonText,
+        ),
+        onClick = {}
+    )
+}
+
+@Composable
+private fun LogInAgainButton(hasLoggedInWithError: Boolean, logIn: () -> Unit) {
+    val loginButtonColor = if (hasLoggedInWithError) {
+        ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = AuthenticatorTheme.colors.accountErrorLoginButtonText,
+        )
+    } else {
+        ButtonDefaults.buttonColors(
+            containerColor = AuthenticatorTheme.colors.loginAgainButton,
+            contentColor = AuthenticatorTheme.colors.loginAgainButtonText,
+        )
+    }
+
+    LargeButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = Margin.Medium)
+            .padding(bottom = Margin.Medium),
+        title = stringResource(R.string.logInButton),
+        colors = loginButtonColor,
+        onClick = {
+            // TODO An error appear if we already tried to log in on figma so for now, we're doing like this
+            logIn()
+        }
+    )
+}
+
+@Composable
+private fun SettingsSections(securityLevel: AccountSecurityLevel) {
+    val firstSectionItem = if (securityLevel == AccountSecurityLevel.Secured) {
+        listOf(
+            OptionItem.WithRightIcon(
+                stringResId = R.string.refreshPendingLoginsButton,
+                rightIconResId = R.drawable.right_indicator
+            ),
+        )
+    } else listOf()
+
+    val secondSectionItems = listOf(
+        OptionItem.WithRightIcon(
+            stringResId = R.string.activityHistoryButton,
+            rightIconResId = R.drawable.square_arrow_up
+        ),
+        OptionItem.WithRightIcon(
+            stringResId = R.string.accountSettingsButton,
+            rightIconResId = R.drawable.square_arrow_up
+        ),
+        OptionItem.Default(
+            stringResId = R.string.disconnectButton,
+            textColor = AuthenticatorTheme.materialColors.error,
+        ),
+    )
+
+    OptionsSection(firstSectionItem, secondSectionItems)
+}
+
+private enum class AccountStatus(
+    val titleResId: Int,
+    val descriptionResId: Int? = null,
+    val iconResId: Int,
+    val iconTint: @Composable (() -> Color)? = null
+) {
+    Secured(
+        titleResId = R.string.accountProtected,
+        iconResId = R.drawable.shield_check,
+        iconTint = { AuthenticatorTheme.colors.accountSecured }),
+    PartiallyProtected(
+        titleResId = R.string.accountPartiallyProtectedTitle,
+        descriptionResId = R.string.accountPartiallyProtectedDescription,
+        iconResId = R.drawable.shield_warning,
+        iconTint = { AuthenticatorTheme.colors.accountWarning }),
+    Disconnected(
+        titleResId = R.string.disconnectSuccess,
+        iconResId = R.drawable.circle_cross,
+        iconTint = { AuthenticatorTheme.colors.accountDisconnected });
+
+    companion object {
+
+        fun from(level: AccountSecurityLevel): AccountStatus {
+            //TODO check how a disconnected account is displayed on the accounts list
+            return when (level) {
+                AccountSecurityLevel.Secured -> Secured
+                AccountSecurityLevel.Warning -> PartiallyProtected
+                else -> Disconnected
+            }
+        }
+    }
+}
+
+@PreviewSmallWindow
+@Composable
+fun AccountDetailsPreview() {
+    AuthenticatorTheme {
+        AccountDetails(
+            FakeAccount(
+                name = "Laura Snow",
+                email = "laura.snow.ik.me",
+                securityLevel = AccountSecurityLevel.Warning,
+            ),
+            onBackPressed = {},
+        )
+    }
+}
