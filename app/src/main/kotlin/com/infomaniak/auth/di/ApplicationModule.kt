63fb83c291a67539272e9696d339d951930c9792
Vincent Te <vincent.te@infomaniak.com>
2026-02-27 13:03:59 +0100

    fix: Rebase

--- app/src/main/kotlin/com/infomaniak/auth/di/ApplicationModule.kt
+++ app/src/main/kotlin/com/infomaniak/auth/di/ApplicationModule.kt
@@ -32,7 +32,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object ApplicationModule {
-    
+
     @Provides
     @Singleton
     fun providesInfomaniakLogin(@ApplicationContext appContext: Context): InfomaniakLogin {
