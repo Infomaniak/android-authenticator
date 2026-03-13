827132acac9b440354c4ed04de1dbaa6f9944fa1
Jamy Bailly <jamy.bailly@infomaniak.com>
2026-03-13 15:56:14 +0100

    fix(AppLock): Remove useless val definition

--- app/src/main/kotlin/com/infomaniak/auth/ui/screen/main/MainViewModel.kt
+++ app/src/main/kotlin/com/infomaniak/auth/ui/screen/main/MainViewModel.kt
@@ -30,7 +30,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val appSettingsRepository: AppSettingsRepository,
+    appSettingsRepository: AppSettingsRepository,
     private val accountUtils: AccountUtils
 ) : ViewModel() {
 
