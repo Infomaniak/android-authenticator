4745ad3b789c56ffcd1a9c9733d043e91a62f45b
Louis CAD <louis.cognault@infomaniak.com>
2026-03-19 15:57:47 +0100

    fix: Remove no longer necessary code

--- app/src/main/kotlin/com/infomaniak/auth/ui/screen/onboarding/start/OnboardingStartViewModel.kt
+++ app/src/main/kotlin/com/infomaniak/auth/ui/screen/onboarding/start/OnboardingStartViewModel.kt
@@ -58,7 +58,6 @@ class OnboardingStartViewModel @Inject constructor(
         trackAccountEvent(MatomoName.LoggedIn)
         viewModelScope.launch {
             users.forEach { user ->
-                authenticatorFacade.registerPasskey(user.apiToken.accessToken, user.id.toLong())
                 addUserToAuthenticatorDB(user)
                 accountUtils.addUser(user)
             }
