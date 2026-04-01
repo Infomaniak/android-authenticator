3659c1958ae6893d59b05dfc66fcb82e8f88a368
Louis CAD <louis.cognault@infomaniak.com>
2026-04-01 16:00:22 +0200

    chore: Rename OnboardingDone to EverythingReady

--- app/src/main/kotlin/com/infomaniak/auth/ui/screen/main/MainScreen.kt
+++ app/src/main/kotlin/com/infomaniak/auth/ui/screen/main/MainScreen.kt
@@ -92,7 +92,7 @@ private fun handleAppStatus(
     val targetDestination = when (appStatus) {
         is AppStatus.LoginRequired -> NavDestination.Onboarding.Start
         is AppStatus.LoggingIn -> NavDestination.SecuringAccount
-        is AppStatus.OnboardingDone -> NavDestination.Onboarding.Complete(appStatus.proceed)
+        is AppStatus.EverythingReady -> NavDestination.Onboarding.Complete(appStatus.proceed)
         AppStatus.SetupComplete -> NavDestination.Root.Home
     }
 
