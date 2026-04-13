74a5986714f3f8d872257c3e7cd886a013edf3f7
Joris Bodin <joris.bodin@infomaniak.com>
2026-04-13 13:31:25 +0200

    chore(Version): 2.0.0-2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.infomaniak.auth"
         minSdk = androidMinSdk
         targetSdk = appTargetSdk
-        versionCode = 2_00_000_02
+        versionCode = 2_00_000_03
         versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
