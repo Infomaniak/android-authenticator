b44c8eae474d8da4cc301ba1d362d17f9fa5431b
Vincent Te <vincent.te@infomaniak.com>
2026-04-22 11:46:07 +0200

    fix: Avatar is now displayed during migration

--- app/src/main/kotlin/com/infomaniak/auth/ui/components/Avatar.kt
+++ app/src/main/kotlin/com/infomaniak/auth/ui/components/Avatar.kt
@@ -19,6 +19,8 @@ package com.infomaniak.auth.ui.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import coil3.SingletonImageLoader
 import com.infomaniak.auth.lib.Account
 import com.infomaniak.core.auth.models.user.User
 import com.infomaniak.core.avatar.LocalAvatarColors
@@ -26,7 +28,8 @@ import com.infomaniak.core.avatar.components.Avatar
 import com.infomaniak.core.avatar.getBackgroundColorResBasedOnId
 import com.infomaniak.core.avatar.models.AvatarColors
 import com.infomaniak.core.avatar.models.AvatarType
-import com.infomaniak.core.avatar.models.AvatarType.WithInitials
+import com.infomaniak.core.avatar.models.AvatarType.Companion.getUrlOrInitials
+import com.infomaniak.core.avatar.models.AvatarUrlData
 
 @Composable
 fun Avatar(
@@ -51,7 +54,6 @@ fun Avatar(
     )
 }
 
-
 @Composable
 private fun computeAvatarType(
     account: Account,
@@ -70,7 +72,11 @@ private fun computeAvatarType(
             .map { it.first() }
             .joinToString("")
             .uppercase()
-        WithInitials.Initials(initials, avatarColors)
+        getUrlOrInitials(
+            avatarUrlData = AvatarUrlData(account.avatarUrl ?: "", SingletonImageLoader.get(LocalContext.current)),
+            initials = initials,
+            colors = avatarColors
+        )
     } else {
         AvatarType.fromUser(user)
     }
