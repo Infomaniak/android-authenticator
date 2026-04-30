76528c6aa73123e0ee467bfe7d741c5151f26fe1
Louis CAD <louis.cognault@infomaniak.com>
2026-04-30 10:47:33 +0200

    refactor: Rename cause to issue in LoginFailed

--- app/src/main/kotlin/com/infomaniak/auth/ui/screen/accountdetails/ActionRequiredCard.kt
+++ app/src/main/kotlin/com/infomaniak/auth/ui/screen/accountdetails/ActionRequiredCard.kt
@@ -82,9 +82,9 @@ fun ActionRequiredCard(
             )
         }
         is Account.Status.NotConnected.LoginFailed -> {
-            when (status.cause) {
+            when (val issue = status.issue) {
                 is Issue.Retriable -> {
-                    val code = when (val cause = (status.cause as Issue.Retriable).cause) {
+                    val code = when (val cause = issue.cause) {
                         Issue.Retriable.Cause.NetworkIssue -> -1
                         Issue.Retriable.Cause.ServerUnavailable -> 503
                         is Issue.Retriable.Cause.Other -> cause.errorCode
@@ -108,7 +108,7 @@ fun ActionRequiredCard(
                                 title = stringResource(RCore.string.buttonRetry),
                                 style = ButtonStyle.Tertiary,
                                 onClick = {
-                                    val cause = status.cause as? Issue.Retriable
+                                    val cause = status.issue as? Issue.Retriable
                                     cause?.proceed?.invoke(true)
                                 }
                             )
