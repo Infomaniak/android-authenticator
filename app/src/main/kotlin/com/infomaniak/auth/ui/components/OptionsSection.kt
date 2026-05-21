3af8479f30f76250dfe95d8b52f628bf0e1e5967
Jamy Bailly <jamy.bailly@infomaniak.com>
2026-05-21 14:47:21 +0200

    fix: Apply scrollable column for accountdetails and settings

--- app/src/main/kotlin/com/infomaniak/auth/ui/components/OptionsSection.kt
+++ app/src/main/kotlin/com/infomaniak/auth/ui/components/OptionsSection.kt
@@ -31,9 +31,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -88,8 +86,7 @@ private fun OptionsSectionContainer(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(top = Margin.Large)
-            .verticalScroll(rememberScrollState()),
+            .padding(top = Margin.Large),
         verticalArrangement = Arrangement.spacedBy(Margin.Large)
     ) {
         sections.forEach { optionsSection ->
