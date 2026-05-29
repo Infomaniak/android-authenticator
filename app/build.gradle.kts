38e66ab12c3954c1f99d74a33c01e723be85893c
Vincent Te <vincent.te@infomaniak.com>
2026-05-29 09:55:47 +0200

    chore(Version): 2.0.0-10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.infomaniak.auth"
         minSdk = androidMinSdk
         targetSdk = appTargetSdk
-        versionCode = 2_00_000_09
+        versionCode = 2_00_000_10
         versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
