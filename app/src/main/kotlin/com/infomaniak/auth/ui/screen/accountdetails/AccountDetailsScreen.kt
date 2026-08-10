f500ac565f4866265bcee6c8be5bcc0c2628fd55
Louis CAD <louis.cognault@infomaniak.com>
2026-08-10 09:58:39 +0200

    chore: Make accessibility skip empty onClick

--- app/src/main/kotlin/com/infomaniak/auth/ui/screen/accountdetails/AccountDetailsScreen.kt
+++ app/src/main/kotlin/com/infomaniak/auth/ui/screen/accountdetails/AccountDetailsScreen.kt
@@ -41,6 +41,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.semantics.onClick
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -192,6 +194,7 @@ private fun Header(account: Account, user: User?) {
                     onLongClick = { sendBreadcrumbForUser(user?.id?.toLong()) },
                     onClick = {},
                 )
+                .semantics { onClick(label = null, action = null) }
                 .padding(horizontal = Margin.Medium)
                 .size(40.dp)
                 .clip(CircleShape)
